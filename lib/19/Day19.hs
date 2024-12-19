57e49ba001b343a9acea9482000162af24636cb0
Michal Pospěch <michal.pospech@gmail.com>
2024-12-19 15:36:40 +0100

    D19 part 2

--- lib/19/Day19.hs
+++ lib/19/Day19.hs
@@ -8,10 +8,10 @@ import Data.List (isPrefixOf)
 import Data.Map (empty, insert, lookup)
 import Data.Map.Strict (Map)
 import Data.Text (Text, pack)
+import Debug.Trace (trace)
 import Text.Parsec (newline, oneOf, sepBy, string)
 import Text.Parsec.Prim (many)
 import Text.Parsec.Text (Parser)
-import Debug.Trace (trace)
 
 type Towel = String
 
@@ -29,7 +29,9 @@ task1 (towels, patterns) = pack $ show res
   where
     (res, _) = runState (countPossible towels patterns) empty
 
-task2 = undefined
+task2 (towels, patterns) = pack $ show res
+  where
+    (res, _) = runState (countPossibleWays towels patterns) empty
 
 towelsParser :: Parser [Towel]
 towelsParser = many (oneOf ['w', 'u', 'b', 'r', 'g']) `sepBy` string ", "
@@ -37,29 +39,34 @@ towelsParser = many (oneOf ['w', 'u', 'b', 'r', 'g']) `sepBy` string ", "
 patternParser :: Parser [Pattern]
 patternParser = many (oneOf ['w', 'u', 'b', 'r', 'g']) `sepBy` newline
 
-countPossible :: (Monad m, MonadState (Map Pattern Bool) m) => [Towel] -> [Pattern] -> m Int
+countPossible :: (Monad m, MonadState (Map Pattern Int) m) => [Towel] -> [Pattern] -> m Int
 countPossible towels patterns = do
-  res <- mapM (canBeCreated' towels) patterns
-  return $ length $ filter id res
+  res <- mapM (waysToCreate' towels) patterns
+  return $ length $ filter (0 <) res
 
-canBeCreated' :: (Monad m, MonadState (Map Pattern Bool) m) => [Towel] -> Pattern -> m Bool
-canBeCreated' = fix canBeCreated
+countPossibleWays :: (Monad m, MonadState (Map Pattern Int) m) => [Towel] -> [Pattern] -> m Int
+countPossibleWays towels patterns = do
+  res <- mapM (waysToCreate' towels) patterns
+  return $ sum res
 
-canBeCreated :: (Monad m, MonadState (Map Pattern Bool) m) => ([Towel] -> Pattern -> m Bool) -> [Towel] -> Pattern -> m Bool
-canBeCreated _ _ "" = return True
-canBeCreated f towels pattern = do
+waysToCreate' :: (Monad m, MonadState (Map Pattern Int) m) => [Towel] -> Pattern -> m Int
+waysToCreate' = fix waysToCreate
+
+waysToCreate :: (Monad m, MonadState (Map Pattern Int) m) => ([Towel] -> Pattern -> m Int) -> [Towel] -> Pattern -> m Int
+waysToCreate _ _ "" = return 1
+waysToCreate f towels pattern = do
   calculated <- gets (Data.Map.lookup pattern)
   case calculated of
     Just b -> return b
     Nothing -> do
       let validPrefixes = filter (`isPrefixOf` pattern) towels
-      if null validPrefixes then
-        do
-            modify (insert pattern False)
-            return False
+      if null validPrefixes
+        then do
+          modify (insert pattern 0)
+          return 0
         else do
-            let validSuffixes = [p | prefix <- validPrefixes, let p = drop (length prefix) pattern]
-            res <- anyM (f towels) validSuffixes
-            modify (insert pattern res)
-            return res
-
+          let validSuffixes = [p | prefix <- validPrefixes, let p = drop (length prefix) pattern]
+          res <- mapM (f towels) validSuffixes
+          let ways = sum res
+          modify (insert pattern ways)
+          return ways
